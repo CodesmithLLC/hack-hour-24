3cedf63a6631a2282f5d8575aa4d2bda57e190e3
Warlock <joel.climbs@gmail.com>
2018-09-12 09:36:04 -0700

    fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,32 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+    //intitialize fresult array
+    let result = [];
+    let stop = num + 1
 
+    //loop up to num
+    for (let i = 0; i < stop; i++) {
+        //set conditionals 
+        //fizzbuzz = nums divisible by both 3 & 5
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("fizzbuzz");
+        }
+        //fizz = nums divisible by 3
+        else if (i % 3 === 0) {
+            result.push("fizz")
+        }
+        //buzz = nums divisible by 5
+        else if (i % 5 === 0) {
+            result.push("buzz");
+        }
+        //just push the num is neither
+        else {
+            result.push(i);
+        }
+    }
+    console.log(result);
+    return result;
 }
-
+fizzbuzz(16);
 module.exports = fizzbuzz;
