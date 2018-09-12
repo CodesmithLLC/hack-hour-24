e9e0073d29c3c5ada4f6a3c1d757f4d95b5ef31c
Erik Fisher <efishr4@gmail.com>
2018-09-12 09:39:17 -0700

    completed challenge

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,21 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  const output = [];
+  for(let i = 1; i <= num; i++) {
+    output.push(i)
+  }
+  return output.map(function(value) {
+    if(value%3 === 0 && value%5 === 0) {
+      return 'fizzbuzz';
+    } else if(value%5 === 0) {
+      return 'buzz';
+    } else if(value % 3 === 0) {
+      return 'fizz';
+    } else {
+      return value;
+    }
+  });
 }
-
+console.log(fizzbuzz(16));
 module.exports = fizzbuzz;
