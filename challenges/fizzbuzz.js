adb01280971cfb3e516491b7ef87b66feac7883d
Zahaan <zahaanjasani@gmail.com>
2018-09-12 09:36:32 -0700

    fizzbuzz completed

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,27 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  //create an empty array
+  //create a loop
+    //if i is divisible by 3 and 5, push fizzbuzz
+    //else if i is divisible by 3, push fizz
+    //else if i is divisible by 5 push buzz
+    //else push i
+  
+  const fizzbuzzArr = [];
+  
+  for (let i = 0; i <= num; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      fizzbuzzArr.push('fizzbuzz');
+    } else if (i % 3 === 0) {
+      fizzbuzzArr.push('fizz');
+    } else if (i % 5 === 0) {
+      fizzbuzzArr.push('buzz');
+    } else {
+      fizzbuzzArr.push(i);
+    }
+  }
+  return fizzbuzzArr;
 }
 
 module.exports = fizzbuzz;
