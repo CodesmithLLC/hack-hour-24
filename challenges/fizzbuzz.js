941d6b102ad53ea57ea98d16d7ca37d6aabf6a82
Edward Roh <Admin@Eds-MacBook-Pro.local>
2018-09-12 09:41:47 -0700

    hack hour #1

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,23 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  const arr = [];
+  let count = 1;
+  while (count < num+1) {
+    if (count % 5 === 0 && count % 3 === 0) {
+      arr.push('fizzbuzz');
+    } else if (count % 5 === 0) {
+      arr.push('buzz');
+    } else if (count % 3 === 0) {
+      arr.push('fizz');
+    } else {
+      arr.push(count);
+    }
+    count++;
+  }
+  
+  return arr;
 }
 
+console.log(fizzbuzz(13));
 module.exports = fizzbuzz;
