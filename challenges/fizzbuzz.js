075416106973e3e7675fd4b0a5b10435ad007b62
Elliot Kim <elliotkim@Elliots-MacBook-Pro.local>
2018-09-12 10:06:27 -0700

    Completed Hack-Hour-24

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,5 +1,6 @@
 // Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
-// fizzbuzz(16);  -> [ 1,
+console.log(fizzbuzz(16));
+//  -> [ 1,
 //                     2,
 //                     'fizz',
 //                     4,
@@ -17,7 +18,21 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    //output array
+    const outputArr = [];
+    //for loop to iterate through numbers
+    for (let i = 1; i <= num; i++){
+        if (i % 3 === 0 && i % 5 === 0) { //if statement to identify numbers divisible by 3 and 5
+            outputArr.push('fizzbuzz');
+        } else if (i % 3 === 0) {  // else if statement to identify numbers divisible by 3
+            outputArr.push('fizz');
+        } else if (i % 5 === 0) {  // else if statement to identify numbers divisible by 5
+            outputArr.push('buzz');
+        } else { // else push numbers
+            outputArr.push(i);
+        }
+    }
+    return outputArr;
 }
 
 module.exports = fizzbuzz;
