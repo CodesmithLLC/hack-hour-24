4533314991857656a9bd567fed5b33f8810ddea6
Garron Ma <garronma@gmail.com>
2018-09-14 10:47:42 -0700

    Finish draw stairs

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -15,7 +15,40 @@
 
 function drawStairs(n) {
 
+  // create empty array
+  const stairs = [];
+
+  // create function that creates the stars row
+  function stars(num) {
+    let counter = 0;
+    let result = '';
+
+    // add the appropriate amount of stars in row
+    while (counter !== num) {
+      result += '*';
+      counter += 1;
+    }
+
+    // buffer the row with spaces in row
+    let buffer = n - result.length;
+    while (buffer !== 0) {
+      result = ` ${result}`;
+      buffer -= 1;
+    }
+    return result;
+  }
+
+  // looping through n-1, starting at n
+  for (let i = n; i > 0; i -= 1) {
+    const row = stars(i);
+    stairs.unshift(row);
+  }
+
+  stairs.forEach((lvl) => {
+    console.log(lvl);
+  });
 }
 
-
 module.exports = drawStairs;
+
+drawStairs(25);
