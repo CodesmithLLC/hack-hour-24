09dcc630ca0bb91dbd0f0819cac8a7f5386b6a25
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-09-14 09:58:00 -0700

    work for draw-stairs HH

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,18 @@
 */
 
 function drawStairs(n) {
-
+    let k = 0;
+    for (let i = 0; i < n; i++) {
+        str = '';
+        for (let j = 0; j < n - i - 1; j++)
+            str += ' ';
+        for (let j = 0; j < i + 1; j++)
+            str += '*';
+    
+        console.log(str);
+    }
 }
 
+drawStairs(6);
 
 module.exports = drawStairs;
