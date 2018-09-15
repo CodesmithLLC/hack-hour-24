b7bbbdb3dddc1676ea30323ab338f78a6c2e1b77
sratemo <sratemo@gmail.com>
2018-09-15 10:36:13 -0700

    MeeeeeanMode Solution

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,43 @@
 
 
 function modemean(array) {
-
+    return mean(array) === mode(array);
 }
 
+function mean(array){
+    
+    let sum = array.reduce((a,b) => a + b)
+    let mean = Math.floor(sum/array.length)
+     return mean;
+}
+
+function mode(array){
+     let obj = {};
+
+    array.forEach((el) =>{
+        if(obj[el]){
+            obj[el]++
+        } else {
+            obj[el] = 1;
+        }
+    })
+  
+    
+    let objValues = Object.values(obj);
+    let maxVal = objValues.reduce((a,b) => a > b ? a : b);
+  
+    let MaxArray = []; //Array of Max keys
+    for(let prop in obj){
+        if(obj[prop] === maxVal){
+            MaxArray.push(prop);
+        }
+    }
+
+    return Number(MaxArray.reduce((a,b) => a > b ? a : b));  //max value from MaxArray
+}
 module.exports = modemean;
+
+console.log(modemean([2,9,8,9,8,9,8]))
+console.log(mode([2,9,8,9,8,9,8]))
+// console.log(mean([2,9,8,8]))
+// console.log(mean([2,9,8,8]))
