6cfb4e9640e6dd4bc611d1de8c307eb9a2eec87c
Lee Rogers <edwinleerogers@gmail.com>
2018-09-15 09:48:53 -0700

    finish mode-mean challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,29 @@
 
 
 function modemean(array) {
-
+  let mean = array.reduce( (a, b) => a + b) / array.length;
+  let mode;
+  let mostNums = 0;
+  let numCount = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] === array[j]) {
+        numCount++;
+      }
+    }
+    if (numCount > mostNums) {
+      mostNums = numCount;
+      mode = array[i];
+    }
+    if (numCount === mostNums) {
+      if (array[i] > mode) {
+        mode = array[i];
+      }
+    }
+    numCount = 0;
+  }
+  return mode === mean;
 }
+
 
 module.exports = modemean;
