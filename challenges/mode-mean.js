aa50d539426e735368f3b31fdaedf9d2d18b9e9b
Oscar Chan <chanoscar0@gmail.com>
2018-09-15 09:35:17 -0700

    mode-mean.js

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,27 @@
 
 
 function modemean(array) {
-
+  const obj = {};
+  let mean = 0;
+  let mode = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    mean += array[i];
+    if (Object.keys(obj).includes(array[i].toString())) {
+      obj[array[i]] += 1;
+    } else {
+      obj[array[i]] = 1;
+    }
+  }
+  mean = Math.floor(mean / array.length);
+  Object.keys(obj).forEach((elem) => {
+    if (obj[elem] >= mode) {
+      mode = elem;
+    }
+  });
+  return mean.toString() === mode;
 }
+
+const test = [2, 2, 2, 2];
+console.log(modemean(test));
 
 module.exports = modemean;
