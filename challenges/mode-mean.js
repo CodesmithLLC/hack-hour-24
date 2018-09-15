0be187d516cc6a8106aa0cbc307f804cce95ea3a
Edward Roh <Admin@Eds-MacBook-Pro.local>
2018-09-15 09:48:01 -0700

    completed mean-mode

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -12,6 +12,31 @@
 
 function modemean(array) {
 
+  // getting mean
+  const mean = Math.floor(array.reduce((acc, curr) => acc + curr)/array.length);
+
+  // getting mode
+    // creating count object
+      const countObj = {};
+      array.forEach(ele => {
+        countObj[ele] = (countObj[ele] || 0) + 1;
+      });
+    
+    // find numbers that occur the most;
+      const maximum = Math.max(...Object.values(countObj).map(ele => {
+        return ele;
+      }));
+    // find key(s) associated with max number
+      let moder;
+      for (let key in countObj) {
+        if (countObj[key] === maximum && (!moder || moder < key)) {
+          moder = key;
+        }
+      }
+      
+  return (mean === parseInt(moder));
 }
+
+console.log(modemean([1,2,2,3,3]));
 
 module.exports = modemean;
