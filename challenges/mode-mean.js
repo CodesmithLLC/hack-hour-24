b1cdb3649e85a3067bdf6b3833dda2f79489333c
Hari Kim <hari9497@Haris-MacBook-Pro.local>
2018-09-18 09:32:19 -0700

    completed mode mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,22 @@
  *
  */
 
+//input: array
+//output: boolean
 
-function modemean(array) {
+// declare a function 'modemean' taking in input
+//     declare a 'storage' empty object
+//     iterate through the array
+//         if storage object does not have the element as the key value
+//             create a key of the element and the value as 1
+//         else if storage object has a key
+//             increment the value of that key
+//     iterate through 'storage' object
+//         if storage[key] < storage[key + 1]
+//         if the value of the key greater than value of the key + 1
+//             return key of storage object
+//         else if the value of the key is less than the value of the key
 
-}
+function modemean(array) {}
 
 module.exports = modemean;
