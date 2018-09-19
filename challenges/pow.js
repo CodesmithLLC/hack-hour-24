124dd89d3df024c9061de5e786356c90d7a8e2cb
Garron Ma <garronma@gmail.com>
2018-09-19 09:46:59 -0700

    Finish pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,22 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+function pow(base, power, product = 1) {
+  if (power === 0) return product;
+  const newPower = power - 1;
+  return pow(base, newPower, base * product);
 }
 
 module.exports = pow;
+
+// power is the incremeneter
+// 2^3... 1*2, 2*2, 4*2
+
+
+console.log(pow(2, 5));
+// power 2
+// 2 * pow(2, 2)
+// power 1
+// 2 * pow(2, 1)
+// power 0
+// 2 * pow(2, 0)
