6c08a14d6c754e1b9bc8cbc343dbf4070c226d32
Warlock <joel.climbs@gmail.com>
2018-09-19 12:26:17 -0700

    first time using the linter, wow. pure function

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,6 +2,7 @@
  * Use recursion!
  */
 
+// linear recursion
 // function pow(base, power) {
 //   //when power equals 1
 //   //base case return base
@@ -11,20 +12,27 @@
 //   return base * pow(base, --power)
 // }
 
+// tail end recursion
 function pow(base, power) {
+  // declare a const to create closure of the initial base
+  const naturalBase = base;
 
-  let naturalBase = base;
-
-  function recursivePow (base, power) {
-    if (power === 1) return base;
-    base = base * naturalBase;
-    power -= 1;
-    return recursivePow(base, power);
+  // define function which will be recursively called to find the power
+  function recursivePow(product, decrementedPower) {
+    // temporary lets to hold values which will be used within the function to prevent side effecs
+    let currentPower = decrementedPower;
+    let currentProduct = product;
+    // base case returns the product of running the recursive funtion
+    if (decrementedPower === 1) return product;
+    // multiply the currentProduct variable by the const base
+    currentProduct *= naturalBase;
+    // decrement the power to keep track of how many times we have multiplied
+    currentPower -= 1;
+    // return the crecursive call if the power requires further multiplication
+    return recursivePow(currentProduct, currentPower);
   }
-
-  return recursivePow(base, power)
+  // call teh recursive function
+  return recursivePow(base, power);
 }
-
-console.log(pow(2,12500));
 
 module.exports = pow;
