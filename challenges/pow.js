fa8c14ede084989f2d1f48b323f57314ecae45bc
Warlock <joel.climbs@gmail.com>
2018-09-19 10:00:26 -0700

    exploring tail end recusion

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,29 @@
  * Use recursion!
  */
 
+// function pow(base, power) {
+//   //when power equals 1
+//   //base case return base
+//   if (power === 1) return base;
+//   //function
+//   //return recurse, decrement power
+//   return base * pow(base, --power)
+// }
+
 function pow(base, power) {
 
+  let naturalBase = base;
+
+  function recursivePow (base, power) {
+    if (power === 1) return base;
+    base = base * naturalBase;
+    power -= 1;
+    return recursivePow(base, power);
+  }
+
+  return recursivePow(base, power)
 }
+
+console.log(pow(2,12500));
 
 module.exports = pow;
