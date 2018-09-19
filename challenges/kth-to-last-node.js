5a02ea84517e6f537cba3f5e9146582dda46aa62
Paul Gi <paulsgi@PAUL-MAC.local>
2018-09-19 09:16:10 -0700

    hh complete

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,30 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  const result = [];
+  let current = head.next;
+  if (!head || k < 1) {
+    return undefined;    
+  }
+  for (let i = head; i !== null; i = i.next) {
+    result.push(i);
+    console.log(result);
+  }
+  return result[result.length - k].value;
 }
 
+
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(2, a));
