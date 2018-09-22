5bce343651c4111fa16c2bd305e8255954291ae6
g-es <gesun@usc.edu>
2018-09-22 09:30:19 -0700

    string rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -14,9 +14,22 @@
 function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
+console.log(isSubstring('helloa','hello'))
 
 function stringRotation(s1, s2) {
-
+  // length not same, false
+  if (s1.length !== s2.length) return false;
+  // split string2 into 2 parts
+  // but because we don't know the length of each part, we need a for loop to get all posibilities
+  for (let i = 1; i < s2.length; i += 1) { 
+    let temp1 = s2.slice(0,i);
+    let temp2 = s2.slice(i);
+    // if both parts are substrings of string1, return true
+    if (isSubstring(s1,temp1)&&isSubstring(s1,temp2)) {
+      return true;
+    }
+  }
+  return false;
 }
-
+console.log(stringRotation("abcdefg", "fgabcde"))
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
