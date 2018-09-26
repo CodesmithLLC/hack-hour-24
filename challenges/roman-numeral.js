10fb52b6facc2f735cccdef9e643f33757547f34
Oscar Chan <chanoscar0@gmail.com>
2018-09-26 09:48:13 -0700

    Roman-numeral-hack-hour

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,24 @@
  * 
  */
 
+const romanMap = {'M':1000, 'CM': 900, 'D':500, 'CD':400, 'C':100, 'XC': 90, 'L':50, 'XL':40, 'X':10,'IX':9,'V':5,'IV':4,'I':1}
 function romanNumeral(n) {
-
+    let romanNumeral = '';
+    let i = 0;
+    const keys = Object.keys(romanMap);
+    while(n > 0){
+            if(n - romanMap[keys[i]]>=0){
+                romanNumeral += keys[i];
+                n = n-romanMap[keys[i]];
+                i = 0;
+            } else{
+                i++;
+            }
+        }
+    
+    return romanNumeral;
 }
+
+console.log(romanNumeral(104));
 
 module.exports = romanNumeral;
