411144eac16670be3a21031cf454c0295f62c685
Erik Fisher <efishr4@gmail.com>
2018-09-26 10:01:05 -0700

    converts number to roman numeral equivalent

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,41 @@
  */
 
 function romanNumeral(n) {
+  const obj = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M"
+  }
+  let output ='';
+  const keysArr = Object.keys(obj)
 
+  while(n > 0) {
+    let greatestNum;
+    if(n >= 1000) {
+      greatestNum = 1000;
+    } else {
+      for(let i = 0; i < keysArr.length; i++) {
+        if(keysArr[i] > n) {
+          greatestNum = keysArr[i-1];
+          break
+        }
+      }
+    }
+    let numTimesNDivsisbleByGreatestNum = Math.floor(n /greatestNum);
+    output += obj[greatestNum].repeat(numTimesNDivsisbleByGreatestNum)
+    n = n - (greatestNum * numTimesNDivsisbleByGreatestNum);
+  }
+  return output;
 }
-
+console.log(romanNumeral(2014))
 module.exports = romanNumeral;
