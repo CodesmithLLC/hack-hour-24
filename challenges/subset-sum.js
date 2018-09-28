cebee80b0cd3a20c4f91151d1c986c15dbd856d5
g-es <gesun@usc.edu>
2018-09-28 10:01:48 -0700

    subset

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,8 +8,31 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-function subsetSum(array, target) {
+function combination (arr) {
 
-}
+    let i, j, temp
+    let result = []
+    let arrLen = arr.length
+    let power = Math.pow
+    let combinations = power(2, arrLen)
+    
+    for (i = 0; i < combinations;  i++) {
+      temp = 0;
+      
+      for (j = 0; j < arrLen; j++) {
+        if ((i & power(2, j))) {
+          temp += arr[j]
+        }
+      }
+      result.push(temp)
+    }
+    return result
+  }
+  
+  console.log(combination([1, 2, 3]))
+  function subsetSum(array, target){
+    return combination(array).includes(target);
+  }
+  console.log(subsetSum([1,2,3],7))
 
-module.exports = subsetSum;
+// module.exports = subsetSum;
