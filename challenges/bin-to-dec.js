e417ae58184a508dbe640f2a7570d05e9fc7d441
g-es <gesun@usc.edu>
2018-09-29 15:39:51 -0700

    bin-to-dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -13,8 +13,27 @@
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
  */
 
+// console.log(parseInt(101,2))
+
+// return num.split('').reverse().reduce(function(x, y, i){
+//   return (y === '1') ? x + Math.pow(2, i) : x;
+// }, 0);
+
 function binToDec(binary) {
-
+  const real = Number(binary.slice(0, binary.length));
+  let sum = 0;
+  function inner(n) {
+    while (n > 1) {
+      const pow10 = Math.floor(Math.log10(n));
+      sum += 2 ** pow10;
+      n -= 10 ** pow10;
+    }
+    if (n === 1) {
+      sum += 1;
+    }
+    return sum;
+  }
+  return inner(real);
 }
-
+console.log(binToDec('1000010'));
 module.exports = binToDec;
