fe97650f9dba4b593a689902be63bbfa5e894e1f
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-10-02 09:57:08 -0700

    match-word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -10,8 +10,47 @@
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
-function matchWord(str) {
-
+function matchInverse(tok1, tok2) {
+    if (tok1.length != tok2.length)
+        return false;
+    for (let i = 0; i < tok1.length; i++) {
+        if (tok1[i].toLowerCase() != tok2[tok2.length - i - 1].toLowerCase())
+            return false;
+    }
+    return true;
 }
 
+function isLetter(input) {
+    var letters = /^[A-Za-z]+$/;
+    if (input.value.match(letters)) {
+        return true;
+    }
+    else {
+        alert("message");
+        return false;
+    }
+}
+function matchWord(str) {
+    if (str === '')
+        return false;
+    let regEx = /[A-Za-z]+/g;
+    let tokArr = str.match(regEx);
+    let tokStack = [];
+    for (let i = 0; i < tokArr.length; i++) {
+        if (tokStack.length > 0 && matchInverse(tokArr[i], tokStack[tokStack.length - 1]))
+            tokStack.pop();
+        else {
+            tokStack.push(tokArr[i]);
+            //console.log(tokStack);
+        }
+    }
+    return tokStack.length === 0;
+}
+
+console.log(matchWord('__END_DNE-----'));
+console.log(matchWord('__ENDDNE__'));
+console.log(matchWord('IF()()fi[]'));
+console.log(matchWord('for__if__rof__fi'));
+console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));
+console.log(matchWord(''));
 module.exports = matchWord;
