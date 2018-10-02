4a45add1523771be227a3bb7fb432be9a24239a8
Serge Vartanov <vartanov.s@gmail.com>
2018-10-02 10:01:47 -0700

    Match Word Challenge Complete

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -3,11 +3,11 @@
  * Write a function that converts the binary string to a decimal number
  *
  * Example:
-* 	binToDec('0')   -> 0
- * 	binToDec('11')  -> 3
- * 	binToDec('100') -> 4
- * 	binToDec('101') -> 5
- *  binToDec('0101') -> 5
+ * binToDec('0')   -> 0
+ * binToDec('11')  -> 3
+ * binToDec('100') -> 4
+ * binToDec('101') -> 5
+ * binToDec('0101') -> 5
  *
  * Extension:
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
@@ -28,9 +28,24 @@ function binToDec(binary) {
   return dec;
 }
 
+function decToBin(dec) {
+  // Declare an array to hold binary values
+  const binArray = [];
+  // Edge case - decimal is 0
+  if (dec === 0) return '0';
+  // Divide decimal by two, adding the remainder to the binary array
+  while (dec > 0) {
+    const bit = String(dec % 2);
+    dec = Math.floor(dec / 2);
+    binArray.unshift(bit);
+  }
+  // Return binary array as string
+  return binArray.join('');
+}
+
 module.exports = binToDec;
 
-// console.log('TESTING balancedParens');
+// console.log('TESTING binToDec');
 // console.log(`binToDec('0'): expect -> 0: actual -> ${binToDec('0')}`);
 // console.log(`binToDec('1'): expect -> 1: actual -> ${binToDec('1')}`);
 // console.log(`binToDec('10'): expect -> 2: actual -> ${binToDec('10')}`);
@@ -40,3 +55,13 @@ module.exports = binToDec;
 // console.log(`binToDec('0101'): expect -> 5: actual -> ${binToDec('0101')}`);
 // console.log(`binToDec('10101'): expect -> 21: actual -> ${binToDec('10101')}`);
 // console.log(`binToDec('101010'): expect -> 42: actual -> ${binToDec('101010')}`);
+
+// console.log('TESTING decToBin');
+// console.log(`decToBin(0): expect -> '0': actual -> ${decToBin(0)}`);
+// console.log(`decToBin(1): expect -> '1': actual -> ${decToBin(1)}`);
+// console.log(`decToBin(2): expect -> '10': actual -> ${decToBin(2)}`);
+// console.log(`decToBin(3): expect -> '11': actual -> ${decToBin(3)}`);
+// console.log(`decToBin(4): expect -> '100': actual -> ${decToBin(4)}`);
+// console.log(`decToBin(5): expect -> '101': actual -> ${decToBin(5)}`);
+// console.log(`decToBin(21): expect -> '10101': actual -> ${decToBin(21)}`);
+// console.log(`decToBin(42): expect -> '101010': actual -> ${decToBin(42)}`);
--- challenges/match-word.js
+++ challenges/match-word.js
@@ -1,17 +1,53 @@
-// Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac", 
-//i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that 
-//takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or 
-//punctuation.
+// Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac",
+// i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that
+// takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or
+// punctuation.
 
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
 // matchWord('IF()()fi[]');  -> true        (should be case-insensitive)
-// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
+// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ]
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
 function matchWord(str) {
+  // Create empty stack to hold words and empty string to hold the current word
+  const stack = [];
 
+  // Helper function to reverse word strings
+  const reverseWord = function reverseWord(word) {
+    return word.split('').reverse().join('');
+  };
+
+  // Trim string, removing all special characters and replacing with spaces
+  const cleanStr = str.replace(/[^a-zA-Z]/g, ' ').trim();
+
+  // Turn string into array of words, filtering out all spaces
+  const cleanStrArr = cleanStr.split(' ').filter(el => el !== '');
+
+  // Loop through the array, checking each word against the stack
+  for (let i = 0; i < cleanStrArr.length; i += 1) {
+    if (!stack.length) {
+      // If the stack is empty, push the word
+      stack.push(reverseWord(cleanStrArr[i]));
+    } else if (cleanStrArr[i].toLowerCase() === stack.slice(-1)[0].toLowerCase()) {
+      // If the word matches the top of the stack, pop the most recent word off the stack
+      stack.pop();
+    } else {
+      // Otherwise, push the reverse of the word to the stack
+      stack.push(reverseWord(cleanStrArr[i]));
+    }
+  }
+  // Once loop complete, check if stack is empty - if so, return true, else return false
+  return !stack.length;
 }
 
 module.exports = matchWord;
+
+// console.log('---TESTING matchWord---');
+// console.log(`matchWord(''): expect -> true: actual -> ${matchWord('')}`);
+// console.log(`matchWord('_END_DNE_'): expect -> true: actual -> ${matchWord('_END_DNE_')}`);
+// console.log(`matchWord('_ENDDNE_'): expect -> false: actual -> ${matchWord('_ENDDNE_')}`);
+// console.log(`matchWord('IF()()fi[]'): expect -> true: actual -> ${matchWord('IF()()fi[]')}`);
+// console.log(`matchWord('for__if__rof__fi'): expect -> false: actual -> ${matchWord('for__if__rof__fi')}`);
+// console.log(`matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'): expect -> true: actual -> ${matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw')}`);
