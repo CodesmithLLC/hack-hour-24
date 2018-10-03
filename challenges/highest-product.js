43ec76aa0976363ef85e1b4d8ae46854386c2c68
Erik Fisher <efishr4@gmail.com>
2018-10-03 09:53:25 -0700

    returns greatest product of 3 nums in array

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,10 @@
  */
 
 function highestProduct(array) {
-
+  const sortedArr = array.sort((a, b) => a > b);
+  const negatives = sortedArr[0] * sortedArr[1] * sortedArr[sortedArr.length - 1];
+  const positives = sortedArr[sortedArr.length - 1] * sortedArr[sortedArr.length - 2] * sortedArr[sortedArr.length - 3];
+  return positives > negatives ? positives : negatives;
 }
 
 
