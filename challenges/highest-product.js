0c5473d8e1982f1bb3c171fb4158c840d26a2f58
g-es <gesun@usc.edu>
2018-10-03 09:50:24 -0700

    highest-pro

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,26 @@
  */
 
 function highestProduct(array) {
-
+  allThree = [];
+  function allPosible(arr, pro=[]){
+    if(arr.length === 0) {
+        if(pro.length===3){
+            allThree.push(pro);
+            // console.log(pro,multiplyAll(pro))
+        }  
+    }
+    else{
+      allPosible(arr.slice(1),pro.concat(arr[0]));
+      allPosible(arr.slice(1),pro);
+    }
+  }
+  allPosible(array);
+  return Math.max(...allThree.map(i=>multiplyAll(i)));
+}
+function multiplyAll(ar){
+  return ar.reduce((accu,curr)=>{return accu*curr},1)
 }
 
+// console.log(highestProduct([1,22,-3,-20,-5,-8,13]));
 
 module.exports = highestProduct;
