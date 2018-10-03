c03135b5102d20002e9cff0772b0ff5bcbeb6412
Edward Roh <Admin@Eds-MacBook-Pro.local>
2018-10-03 09:35:43 -0700

    highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,9 @@
  */
 
 function highestProduct(array) {
-
+  const sort = array.sort();
+  return sort[sort.length-1] * sort[sort.length-2] * sort[sort.length-3];
 }
 
-
+console.log(highestProduct([2,5,4,7]))
 module.exports = highestProduct;
