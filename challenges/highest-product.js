65bfeef6e9604760f90119a7704c51e9951b284a
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-10-04 09:15:03 -0700

    highestProduct bug fix

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -5,10 +5,12 @@
 function highestProduct(array) {
     array.sort((a, b) => a > b);
     let arrLen = array.length;
-    return array[arrLen - 1] * array[arrLen - 2] * array[arrLen - 3];
+    let biggest =  array[arrLen - 1] * array[arrLen - 2] * array[arrLen - 3];
+    let smallest = array[0] * array[1] * array[arrLen - 1];
+    return Math.max(biggest, smallest);
 }
 
-console.log(highestProduct([3, 2,5,4]));
+console.log(highestProduct([-50, -1, 2, 10, 80]));
 
 
 module.exports = highestProduct;
