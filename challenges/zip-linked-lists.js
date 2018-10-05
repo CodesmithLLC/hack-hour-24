53971aff2d886acbc3ebc9088414ae9399d2ef70
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-10-05 11:01:31 -0700

    zip list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,41 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  let cur1 = l1;
+  let cur2 = l2;
+  while(cur1 != null && cur2 != null) {
+    let x = cur1;
+    cur1 = cur1.next;  
+    let y = cur2;
+    cur2 = cur2.next;
+    y.next = x.next;
+    x.next = y;  
+  }
+  return l1;
 };
+
+let arr1 = ['c', 'd', 's', 'i', 'h'];
+let arr2 = ['o', 'e', 'm', 't'];
+
+function makeLlist(arr) {
+  let llist = null;
+  var cur; 
+  for (let i = 0; i < arr.length; i++) {
+    let newNode = new Node(arr[i]);
+    if (llist == null) {
+      cur = llist = newNode;
+    } else {
+      cur.next = newNode;
+      cur = newNode;
+    }
+  }
+  return llist;
+}
+
+let l1 = makeLlist(arr1);
+let l2 = makeLlist(arr2);
+let l1l2 = zip(l1, l2);
+
+
 
 module.exports = {Node: Node, zip: zip};
