9ab4894ac69301df13f8df249023ac802efdac6f
Serge Vartanov <vartanov.s@gmail.com>
2018-10-05 10:00:54 -0700

    Zip Linked Lists Attempted

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,27 @@ function Node(val) {
   this.next = null;
 }
 
-function zip(l1, l2) {
+const zip = function zip(l1, l2) {
+  // Declare mutable pointers to l1 and l2
+  let currNode1 = l1;
+  let currNode2 = l2;
+
+  while (l1.next || l2.next) {
+    // Declare block-scoped pointers to l1.next and l2.next
+    const zipNode1 = currNode1.next;
+    const zipNode2 = currNode2.next;
+    // Point currNode1 to l2 (without losing l1.next)
+    currNode1.next = l2;
+    // If zipNode1 isn't null, point currNode2 at zipNode1 and move currNode1 to zipNode1
+    if (zipNode1) {
+      currNode2.next = zipNode1;
+      currNode1 = zipNode1;
+    }
+    // If zipNode2 exists, migrate currNode2 to zipNode2
+    if (zipNode2) {
+      currNode2 = zipNode2;
+    }
+  }
 };
 
 module.exports = {Node: Node, zip: zip};
