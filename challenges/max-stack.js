74cf3fafc581e1bcd0d17f34c9a7d692397fd774
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-10-06 09:39:14 -0700

    max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,30 @@
 
 function Stack() {
   // body...
+  let obj = {};
+  let length = 0;
+  let max = Number.MIN_SAFE_INTEGER;
+
+  this.push = (value) => {
+    max = Math.max(value, max);
+    obj[length++] = { value, max }
+  }
+  this.pop = () => {
+    let ret = obj[--length].value;
+    max = obj[length - 1].max;
+    return ret;
+  }
+  this.getMax = () => {
+    return max;
+  }
 }
+
+let s = new Stack();
+s.push(1);
+s.push(3);
+console.log(s.getMax());
+s.push(2);
+console.log(s.pop());
+console.log(s.getMax());
 
 module.exports = Stack;
