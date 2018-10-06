2a5f97308eed4da28cb942e7915cba375f7d2420
Erik Fisher <efishr4@gmail.com>
2018-10-06 09:58:00 -0700

    stack with pop, push and getMax methods

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,36 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {};
+  this.index = 0;
+  this.max = undefined;
 }
+Stack.prototype.push = function push(val) {
+  this.storage[this.index] = val;
+  this.index += 1;
+  if (this.max === undefined) {
+    this.max = val;
+  } else if (val > this.max) {
+    this.max = val;
+  }
+  return val;
+};
+
+Stack.prototype.pop = function pop() {
+  const returnVal = this.storage[this.index - 1];
+  delete this.storage[this.index - 1];
+  this.index -= 1;
+  if (this.max === returnVal && this.index === 1) {
+    this.max = undefined;
+  } else if (this.max === returnVal) {
+    const values = Object.values(this.storage);
+    this.max = Math.max(...values);
+  }
+  return returnVal;
+};
+
+Stack.prototype.getMax = function getMax() {
+  return this.max;
+};
 
 module.exports = Stack;
