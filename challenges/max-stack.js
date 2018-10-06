9639d1a4491f720e182a1489bc2a2b71df8ee735
g-es <gesun@usc.edu>
2018-10-06 09:50:39 -0700

    max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,46 @@
 
 function Stack() {
   // body...
+  this.storage = {};
+  this.index = 0;
 }
+
+Stack.prototype.top = undefined;
+Stack.prototype.push = function push(val) {
+  this.storage[this.index] = val;
+  this.index += 1;
+
+  // if (this.top === undefined || val > this.top) {
+  //   this.top = val;
+  // }
+
+  return this.index;
+};
+Stack.prototype.pop = function pop() {
+  this.index -= 1;
+  const temp = this.storage[this.index];
+  delete this.storage[this.index];
+
+  // if (this.top === this.storage[this.index]){
+    // I still have to use O(n) here...
+  //   this.top = Math.max()
+  // }
+  
+  return temp;
+};
+Stack.prototype.getMax = function max() {
+
+  // but Object.keys, Object.values are O(n).......
+  return Math.max(...Object.values(this.storage));
+
+  // return this.top;
+};
+const newS = new Stack();
+newS.push(2);
+newS.push(50);
+newS.push(12);
+newS.pop();
+newS.pop();
+console.log(newS.getMax())
 
 module.exports = Stack;
