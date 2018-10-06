bf030218a543aa1096023ede80b27cc52e63021b
Serge Vartanov <vartanov.s@gmail.com>
2018-10-06 09:29:39 -0700

    Max Stack Challenge Complete

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,38 @@
  */
 
 function Stack() {
-  // body...
+  this.contents = {};
+  this.length = 0;
 }
 
+Stack.prototype.push = function push(el) {
+  this.contents[this.length] = el;
+  this.length += 1;
+  return this.length;
+};
+
+Stack.prototype.pop = function pop() {
+  if (this.length > 0) {
+    this.length -= 1;
+    const popEl = this.contents[this.length];
+    delete this.contents[this.length];
+    return popEl;
+  }
+  return undefined;
+};
+
+Stack.prototype.getMax = function getMax() {
+  return Math.max(...Object.values(this.contents));
+};
+
 module.exports = Stack;
+
+// console.log('---TESTING STACK---');
+// const myStack = new Stack();
+// myStack.push(5);
+// myStack.push(20);
+// myStack.push(10);
+// myStack.push(15);
+// console.log(myStack.pop());
+// console.log(myStack);
+// console.log(myStack.getMax());
