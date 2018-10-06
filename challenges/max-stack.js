90cd87e7e8a537cf3928d74a9eae435a3d20e7c0
Lee Rogers <edwinleerogers@gmail.com>
2018-10-06 10:36:21 -0700

    max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,39 @@
  */
 
 function Stack() {
-  // body...
+  this.stack = {};
+  this.index = 0;
+  this.max = [];
 }
+
+Stack.prototype.push = function(num) {
+  if (this.max.length === 0 || num >= this.max[this.max.length-1]) {
+    this.max.push(num);
+  }
+  this.stack[this.index] = num;
+  this.index += 1;
+  return this.index;
+}
+
+Stack.prototype.pop = function() {
+  if (this.stack[this.index - 1] === this.max[this.max.length-1]) {
+    this.max.pop();
+  }
+  this.index -= 1;
+  let returnVal = this.stack[this.index];
+  delete this.stack[this.index];
+  return returnVal;
+}
+
+Stack.prototype.return = function() {
+  return this.stack;
+}
+
+Stack.prototype.getMax = function() {
+  return this.max[this.max.length - 1];
+}
+
+let newStack = new Stack();
+
 
 module.exports = Stack;
