046949f3759bf2e6363dfe62d1ddbcaea8935b61
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-10-09 09:47:02 -0700

    reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,32 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    let prev = null;
+    let cur = head;
+    while(cur != null) {
+        let more = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = more;
+    }
+    return prev;
 }
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+node1.next = node2;
+let node3 = new Node(3);
+node2.next = node3;
+
+function traverse(head) {
+    let cur = head;
+    while (cur != null) {
+        console.log(cur.value);
+        cur = cur.next;
+    }
+}
+traverse(node1);
+let newHead = reverseLinkedList(node1);
+traverse(newHead);
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
