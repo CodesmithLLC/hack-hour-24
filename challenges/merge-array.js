9e7842ae0cf488e141cb84e8407a016c4ad222ec
g-es <gesun@usc.edu>
2018-10-11 09:50:28 -0700

    merge-array

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,26 @@
  */
 
 function mergeArrays(arr1, arr2) {
-
+  let L1 = arr1.length;
+  let L2 = arr2.length;
+  let L = L1<L2?L1:L2
+  let arr = [];
+  // when i use while loop, it didn't work???
+  for (let i = 0; i < 2*L; i += 1) {
+    // console.log(arr1[0],arr2[0]);
+    if (arr1[0] <= arr2[0]){
+      arr.push(arr1[0]);
+      arr1 = arr1.slice(1);
+    }
+    else if (arr1[0] > arr2[0]){
+      arr.push(arr2[0]);
+      arr2 = arr2.slice(1);
+    }
+    // console.log(arr)
+  }
+  arr = arr.concat(arr2).concat(arr1);
+  return arr;
 }
-
+console.log(mergeArrays([1,4,6], [0,2,5,7,10]))
+// console.log(mergeArrays([3,4,6,10,11,15,21], [1,5,8,12,14,19]))
 module.exports = mergeArrays;
