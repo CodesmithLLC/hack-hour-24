fcb0ba1c4c4a548e1452b1357c3d4e1b5ac3d728
joelkperkins <joel.climbs@gmail.com>
2018-10-13 09:54:54 -0700

    to see the answer, consider the question

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,30 @@
  */
 
 function permPalin(str) {
-	
+  // if string.len is even, must have the same amount of every letter
+  if (str.length === 1) return true;
+
+  let workArr = Array.from(str);
+  let cache = {};
+
+  workArr.forEach(item => {
+    if (cache[item]) cache[item] += 1;
+    else cache[item] = 1;
+  });
+  const palCheck = Object.values(cache);
+
+  if (str.length % 2 === 0) {
+    for (let i = 1; i < palCheck.length; i += 1) {
+      if (palCheck[0] !== palCheck[i]) return false;
+    }
+    return true;
+  } else {
+    palCheck.sort((a, b) => { a - b});
+    for (let i = 2; i < palCheck.length; i += 1) {
+      if (palCheck[1] !== palCheck[i]) return false
+    }
+    return true;
+  }
 }
 
 module.exports = permPalin;
