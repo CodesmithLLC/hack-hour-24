41cbfd7bd963ccd21db0e3a373aa99af67dbf556
g-es <gesun@usc.edu>
2018-10-15 09:29:31 -0700

    rotate-grid

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -16,8 +16,32 @@
  * BONUS: Do this in place
  */
 
+ // 11 12 13 -> 31 32 33
+ // 21 22 23 -> 12 22 32
+ // 31 32 33 -> 11 21 31
+
+ // 12
+ // 34
+
+ // 31
+ // 42
 function rotateGrid(grid, n) {
+  if (n > grid.length) return undefined 
+  const arrBig = [];
+  for (let i = 0; i < n; i += 1) {
+    arrBig[i] = [];
+  }
 
+  for (let i = 0; i < n; i += 1) {
+    for (let j = n - 1; j >= 0; j -= 1) {
+      arrBig[i].push(grid[j][i]);
+    }
+  }
+  return arrBig;
 }
-
+console.log(rotateGrid([   [1, 2,3,4],
+                           [5,6,7,8],
+                           [9,10,11,12],
+                           [13,14,15,16]
+                           ], 5))
 module.exports = rotateGrid;
