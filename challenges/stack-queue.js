7b3e0ed5b81513134873235d8c51521ae773ca14
g-es <gesun@usc.edu>
2018-10-16 09:48:37 -0700

    queue with 2 sstacks

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,9 +4,19 @@
 
 
 function Stack() {
-
+  this.storage = {};
+  this.index = 0;
 }
-
+Stack.prototype.push = function push(val) {
+  this.storage[this.index] = val;
+  this.index ++;
+}
+Stack.prototype.pop = function pop(val) {
+  this.index --;
+  const temp = this.storage[this.index];
+  delete this.storage[this.index];
+  return temp;
+}
 
 /**
 * Queue Class
@@ -14,7 +24,36 @@ function Stack() {
 
 
 function Queue() {
+  this.storage1 = new Stack();
+  this.storage2 = new Stack();
 
 }
+Queue.prototype.enqueue = function pu(val) {
+  this.storage1.push(val)
+}
+Queue.prototype.dequeue = function pu(val) {
 
+  // console.log(this.storage1,'1')
+  // check if storage2 is empty, can't ignore this step...
+  if (this.storage2.index === 0) {
+    if (this.storage1.index === 0)  return undefined; 
+    while (this.storage1.index > 0) {
+      //  get rid of the last one in storage1, push it to storage2 as its first element
+      const temp = this.storage1.pop();
+      this.storage2.push(temp);
+      // console.log(this.storage2,'2')
+    }
+  }
+  return this.storage2.pop();
+}
+const testQ = new Queue();
+testQ.enqueue(3);
+testQ.dequeue();
+testQ.enqueue(4);
+testQ.enqueue(5);
+testQ.enqueue(6);
+testQ.dequeue();
+testQ.dequeue();
+
+// console.log(testQ)
 module.exports = {Stack: Stack, Queue: Queue};
