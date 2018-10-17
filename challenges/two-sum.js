29594881758dfa5c3cb5a49b5d519ad0cc47a93a
Oscar Chan <chanoscar0@gmail.com>
2018-10-17 09:37:34 -0700

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,16 @@
  */
 
 function twoSum(arr, n) {
-
+  for (let i = 0; i < arr.length; i += 1){
+    if(arr.includes(n-arr[i])){
+      return true;
+    }
+  }
+  return false;
 }
+
+let test = [1,2,3,4,5,6,7]
+
+console.log(twoSum(test, 12));
 
 module.exports = twoSum;
