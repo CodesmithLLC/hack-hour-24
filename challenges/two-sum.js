147c3e34039bb768578e6133187aa1cded03a994
g-es <gesun@usc.edu>
2018-10-17 09:35:29 -0700

    twoSum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,14 @@
  */
 
 function twoSum(arr, n) {
-
+  const nums = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    nums.push(n - arr[i]);
+  }
+  for (let j = 0; j < arr.length; j += 1) {
+    if (nums.indexOf(arr[j]) > -1 && nums.indexOf(arr[j]) !== j) return true;
+  }
+  return false;
 }
-
+console.log(twoSum([1,4,5,7], 10))
 module.exports = twoSum;
