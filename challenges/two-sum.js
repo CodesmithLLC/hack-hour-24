ff4e750732408d378eefe54f7ab820eecd49c9fb
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-10-17 09:43:38 -0700

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,21 @@
  */
 
 function twoSum(arr, n) {
-
+    let newArr = arr.sort((a, b) => a - b);
+    let i = 0;
+    let j = arr.length - 1;
+    while(i < j) {
+        let sum = arr[i] + arr[j];
+        if (sum === n)
+            return true;
+        else if (sum < n)
+            i++;
+        else
+            j--;
+    }
+    return false;
 }
+console.log(twoSum([5, 9, 13, 1], 10));
+console.log(twoSum([5, 9, 13, 1], 9));
 
 module.exports = twoSum;
