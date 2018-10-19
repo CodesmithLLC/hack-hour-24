0fd04f8c226e588a84f94803126ae49300ec326c
Kevin Mui <kmui.work@gmail.com>
2018-10-19 10:06:29 -0700

    Completted gcd.

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,15 @@
  */
 
 function gcd(a, b) {
+  const end = Math.min(a, b);
 
+  for (let i = end; i > 0; i -= 1) {
+    if (a % i === 0 && b % i === 0) {
+      return i;
+    }
+  }
 }
+
+// console.log(gcd(10, 9));
 
 module.exports = gcd;
