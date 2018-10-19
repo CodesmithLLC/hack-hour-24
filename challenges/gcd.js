c5dda7514d60bbec172b0de2b2bf5da1f738fdeb
Serge Vartanov <vartanov.s@gmail.com>
2018-10-19 10:12:28 -0700

    GCD Challenge Complete - w/Euclidian Algo

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,17 @@
  */
 
 function gcd(a, b) {
-
+  let i = Math.min(a, b);
+  while (i > 1) {
+    if (!(a % i) && !(b % i)) return i;
+    i = Math.max((a % i), (b % i));
+  }
+  return i;
 }
 
 module.exports = gcd;
+
+console.log('---TESTING gcd---');
+console.log(`gcd(12, 9): expect -> 3: actual -> ${gcd(12, 9)}`);
+console.log(`gcd(10, 8): expect -> 2: actual -> ${gcd(10, 8)}`);
+console.log(`gcd(10, 9): expect -> 1: actual -> ${gcd(10, 9)}`);
