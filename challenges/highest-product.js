fa41f0884ca1e52d231acff1b83d208c6bcff158
Garron Ma <garronma@gmail.com>
2018-10-26 10:58:44 -0700

    finish highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,8 @@
  */
 
 function highestProduct(array) {
-  if (array.length < 3 || !Array.isArray(array)) return 0;
+  if (!Array.isArray(array)) return 0;
+  if (array.length < 3) return 0;
 
   const sortedArray = array.sort((a, b) => a - b);
 
