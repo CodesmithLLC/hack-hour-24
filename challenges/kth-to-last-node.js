f6ef768f521e3a27f6c8b0dad5d7b4c23d06e9e2
Garron Ma <garronma@gmail.com>
2018-10-26 10:32:31 -0700

    finish kth-to-last-node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -16,17 +16,6 @@
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
 
-const a = new Node('A');
-const b = new Node('B');
-const c = new Node('C');
-const d = new Node('D');
-const e = new Node('E');
-
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
-
 function Node(val) {
   this.value = val;
   this.next = null;
