96b672df35327e8c189de39148224d39c8eb5472
Garron Ma <garronma@gmail.com>
2018-10-26 10:51:26 -0700

    finish highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,32 +3,47 @@
  */
 
 function highestProduct(array) {
-  const positivesArr = array.filter(num => num > 0).sort((a, b) => b - a);
-  const negativesArr = array.filter(num => num < 0).sort((a, b) => a - b);
-  const productThree = [];
+  if (array.length < 3) return undefined;
+  // sort
+  const sortedArray = array
+    .sort((a, b) => a - b);
+    // .filter(num => num !== 0);
+  console.log(sortedArray);
 
-  while (productThree.length !== 3) {
-    const biggestNeg = negativesArr[0];
-    const biggestPos = positivesArr[0];
+  // if two neg numbers exist
+  const firstTwoNumsProduct = sortedArray.slice(0, 2).reduce((prod, num) => prod * num);
+  const thirdAndSecondLastNumsProduct = sortedArray.slice(-3, -1).reduce((prod, num) => prod * num);
+  console.log(thirdAndSecondLastNumsProduct);
+  console.log(firstTwoNumsProduct);
 
-    if (Math.abs(biggestNeg) > biggestPos) {
-      productThree.push(biggestNeg);
-      negativesArr.shift();
-    } else {
-      productThree.push(biggestPos);
-      positivesArr.shift();
-    }
+  // check if their product is > product of 3rd to last and 2nd to last numbers (positive)
+  if (firstTwoNumsProduct > thirdAndSecondLastNumsProduct) {
+    // if true, return product of greatest two neg numbers and highest pos number
+    return firstTwoNumsProduct * sortedArray.slice(-1);
   }
-
-  return productThree.reduce((product, num) => product * num);
+  // if false, return product of greatest three pos numbers
+  return thirdAndSecondLastNumsProduct * sortedArray.slice(-1);
 }
 
-console.log(highestProduct([-10, -5, -2, 0, 1, 2, 5]));
+console.log(highestProduct([-3, -2, -1, 0]));
 
 module.exports = highestProduct;
 
+// const positivesArr = array.filter(num => num > 0).sort((a, b) => b - a);
+// const negativesArr = array.filter(num => num < 0).sort((a, b) => a - b);
+// const productThree = [];
 
-// if there is more than one negative
-  // find the largest two negatives greater than the sma
+// while (productThree.length !== 3) {
+//   const biggestNeg = negativesArr[0];
+//   const biggestPos = positivesArr[0];
 
-  // remove zero if exists
+//   if (Math.abs(biggestNeg) > biggestPos) {
+//     productThree.push(biggestNeg);
+//     negativesArr.shift();
+//   } else {
+//     productThree.push(biggestPos);
+//     positivesArr.shift();
+//   }
+// }
+
+// return productThree.reduce((product, num) => product * num);
