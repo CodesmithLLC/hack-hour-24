54c2ba87b83f43fade55994233ec100e9309ef97
Kevin Mui <kmui.work@gmail.com>
2018-10-29 09:52:05 -0700

    Completed count-twos.

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -1,6 +1,6 @@
-// Write a function "countTwos" that takes a number as an argument, n. The function will return the number of 2s encountered when counting from 1 to n (inclusive). 
+// Write a function "countTwos" that takes a number as an argument, n. The function will return the number of 2s encountered when counting from 1 to n (inclusive).
 // For example: countTwos(3) should be 1 because there are no 2s in the number one. There is One 2 in the number two and no 2s in the number three.
-// Each 2 should be counted. For instance,  the number 2222 has four 2s. 
+// Each 2 should be counted. For instance,  the number 2222 has four 2s.
 
 // countTwos(1);  -> 0
 // countTwos(3);  -> 1
@@ -10,7 +10,15 @@
 
 
 function countTwos(num) {
+  let count = 0;
 
+  for (let i = 1; i <= num; i += 1) {
+    count += i.toString().split('').filter(v => v === '2').length;
+  }
+
+  return count;
 }
+
+console.log(countTwos(11420));
 
 module.exports = countTwos;
