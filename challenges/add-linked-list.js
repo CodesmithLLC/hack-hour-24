f503775d7cb4c0e7d9df3c1f6451378946025253
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-10-30 09:38:52 -0700

    add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,48 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let carryOver = 0;
+  let cur = null;
+  let head = null;
+  while (l1 != null || l2 != null) {
+    let digitSum = ((l1 !== null)? l1.value : 0) + ((l2 !== null)? l2.value : 0) + carryOver;
+    carryOver = (digitSum >= 10) ? 1 : 0;
 
+    if (cur === null)
+      head = cur = new Node(digitSum % 10);
+    else {
+      cur.next = new Node(digitSum % 10);
+      cur = cur.next;
+    }
+
+    if (l1 != null)
+      l1 = l1.next;
+    if (l2 != null)
+      l2 = l2.next;
+  }
+  return head;
 }
 
-module.exports = {Node: Node, addLinkedList: addLinkedList};
+function printOut(list) {
+  let cur = list;
+  while(cur != null) {
+    console.log(cur.value, " -> ");
+    cur = cur.next;
+  }
+}
+let l1 = new Node(2);
+let cur1 = l1;
+cur1.next = new Node(1);
+cur1 = cur1.next;
+cur1.next = new Node(8);
+cur1 = cur1.next;
+cur1.next = new Node(5);
+cur1 = cur1.next;
+let l2 = new Node(5);
+let cur2 = l2;
+cur2.next = new Node(9);
+cur2 = cur2.next;
+cur2.next = new Node(2);
+cur2 = cur2.next;
+printOut(addLinkedList(l1, l2));
+module.exports = { Node: Node, addLinkedList: addLinkedList };
