7868380c874eaa853c6f4786969df653c682c942
Serge Vartanov <vartanov.s@gmail.com>
2018-10-30 10:01:20 -0700

    Add Linked List Challenge Complete

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,86 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  // Create new pointers for arguments (avoid argument mutation)
+  let currentL1 = l1;
+  let currentL2 = l2;
 
+  // Initialize carry and pointers to head and tail of output node
+  let carry = 0;
+  let newHead = null;
+  let newTail = null;
+
+  // Helper function to add two values and a carry and set the next carry
+  const addAndCarry = (v1, v2, v3) => {
+    if ((v1 + v2 + v3) >= 10) {
+      // If value is greater than 10, reduce by 10 and set carry to 1
+      carry = 1;
+      return v1 + v2 + v3 - 10;
+    }
+    // If value is less than 10, set carry to 0
+    carry = 0;
+    return v1 + v2 + v3;
+  };
+
+  // If the current node of both lists exists
+  while (currentL1 && currentL2) {
+    // Create new node whose value is currentL1 + currentL2 + carry
+    const newNode = new Node(addAndCarry(currentL1.value, currentL2.value, carry));
+
+    // Set the node and update the tail
+    if (newTail) {
+      newTail.next = newNode;
+      newTail = newNode;
+    } else {
+      // If this is the first new node (ones), point head and tail to it
+      newHead = newNode;
+      newTail = newNode;
+    }
+
+    // Increment the place on both nodes
+    currentL1 = currentL1.next;
+    currentL2 = currentL2.next;
+  }
+
+  // Once loop concludes, check if either l1 or l2 have value
+  while (currentL1) {
+    if (carry) {
+      // If there is a carry, add it to currentL1.value and update carry
+      currentL1.value = addAndCarry(currentL1.value, 0, carry);
+    }
+    // Increment the tail and the place on L1
+    newTail.next = currentL1;
+    newTail = currentL1;
+    currentL1 = currentL1.next;
+  }
+
+  while (currentL2) {
+    if (carry) {
+      // If there is a carry, add it to currentL1.value and update carry
+      currentL2.value = addAndCarry(0, currentL2.value, carry);
+    }
+    // Increment the tail and the place on L1
+    newTail.next = currentL2;
+    newTail = currentL2;
+    currentL2 = currentL2.next;
+  }
+
+  // Check if carry still has value - if so, add one more node to the end and migrate tail
+  if (carry) {
+    newTail.next = new Node(1);
+    newTail = newTail.next;
+  }
+
+  return newHead;
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
+
+// console.log('---TESTING add-linkedlists---');
+// const testL1 = new Node(4);
+// testL1.next = new Node(6);
+// testL1.next.next = new Node(8);
+// const testL2 = new Node(4);
+// testL2.next = new Node(6);
+// testL2.next.next = new Node(8);
+// console.log(addLinkedList(testL1, testL2));
