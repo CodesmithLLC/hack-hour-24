42b6d0c6d0e70ad6ae4083e184ba8139dc5bdea0
Garron Ma <garronma@gmail.com>
2018-10-31 09:43:17 -0700

    finished superbalanced

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,7 +14,45 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+  let leftOfHeadSubTree = tree.left;
+  let rightofHeadSubTree = tree.right;
+  let leftOfHeadTreeDepth, rightOfHeadTreeDepth;
+
+  const findDepth = (subTree, depth) => {
+    console.log(subTree.left)
+    if (subTree.left === null && subTree.right === null) return depth;
+    if (subTree.left === null) return findDepth(subTree.right, depth + 1);
+    if (subTree.right === null) return findDepth(subTree.left, depth + 1);
+    return Math.max(findDepth(subTree.left, depth + 1), findDepth(subTree.right, depth + 1));
+  };
+
+  leftOfHeadTreeDepth = findDepth(leftOfHeadSubTree, 1);
+  console.log(leftOfHeadTreeDepth);
+  rightOfHeadTreeDepth = findDepth(rightofHeadSubTree, 1);
+  console.log(rightOfHeadTreeDepth);
+  return Math.abs(leftOfHeadTreeDepth - rightOfHeadTreeDepth) === 0 || Math.abs(leftOfHeadTreeDepth - rightOfHeadTreeDepth) === 1;
 
 }
 
-module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
+module.exports = { BinaryTree: BinaryTree, superbalanced: superbalanced };
+
+const root = new BinaryTree(5);
+const three = new BinaryTree(3);
+const one = new BinaryTree(1);
+root.left = three;
+three.left = one;
+
+const eight = new BinaryTree(8);
+const fifteen = new BinaryTree(15);
+const eleven = new BinaryTree(11);
+const twelve = new BinaryTree(12);
+root.right = eight;
+eight.right = fifteen;
+fifteen.left = eleven;
+eleven.right = twelve;
+
+console.log(root)
+
+
+
+console.log(superbalanced(root));
