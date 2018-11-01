007fdd1f795eb23efea8f64de0294eb843e2809a
Oscar Chan <chanoscar0@gmail.com>
2018-11-01 09:33:09 -0700

    repeat-number.js

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,22 @@
  */
 
 function repeatNumbers(array) {
-
+  let output = {};
+  for (let i = 0; i < array.length; i += 1) {
+    if (output.hasOwnProperty(String(array[i]))) {
+      output[array[i]] += 1;
+    }
+    else {
+      output[array[i]] = 1;
+    }
+  }
+  for (let key in output) {
+    if(output[key] === 2) return key;
+  }
 }
+
+let testArr = [1,2,3,4,5,6,7,8,9,8, 10];
+
+console.log(repeatNumbers(testArr));
 
 module.exports = repeatNumbers;
