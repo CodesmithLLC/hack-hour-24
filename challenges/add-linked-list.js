6d79681f4a86a77a8968d86de7936801b84258b4
Kevin Mui <kmui.work@gmail.com>
2018-11-01 09:19:09 -0700

    FIxed add-linked-list, completed repeat-numbers.

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,77 +18,122 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
-  // let curNode1 = l1;
-  // let curNode2 = l2;
-  // let carry = 0;
-  // const total = [];
-
-  // while (curNode1 !== null && curNode2 !== null) {
-  //   const sum = curNode1.value + curNode2.value + carry;
-  //   total.push(sum % 10);
-  //   if (sum > 9) {
-  //     carry = 1;
-  //   } else {
-  //     carry = 0;
-  //   }
-  //   curNode1 = curNode1.next;
-  //   curNode2 = curNode2.next;
-  // }
-
-  // while (curNode1 !== null) {
-  //   const sum = curNode1.value + carry;
-  //   total.push(sum % 10);
-  //   if (sum > 9) {
-  //     carry = 1;
-  //   } else {
-  //     carry = 0;
-  //   }
-  // }
-
-  const num1 = [];
-  const num2 = [];
-
   let curNode1 = l1;
   let curNode2 = l2;
+  let carry = 0;
+  let head;
+  let curNode;
+
+  while (curNode1 !== null && curNode2 !== null) {
+    const sum = curNode1.value + curNode2.value + carry;
+
+    if (head === undefined) {
+      head = new Node(sum % 10);
+      curNode = head;
+    } else {
+      curNode.next = new Node(sum % 10);
+      curNode = curNode.next;
+    }
+
+    if (sum > 9) {
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+    curNode1 = curNode1.next;
+    curNode2 = curNode2.next;
+  }
 
   while (curNode1 !== null) {
-    num1.push(curNode1.value);
+    const sum = curNode1.value + carry;
+
+    if (head === undefined) {
+      head = new Node(sum % 10);
+      curNode = head;
+    } else {
+      curNode.next = new Node(sum % 10);
+      curNode = curNode.next;
+    }
+
+    if (sum > 9) {
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+
     curNode1 = curNode1.next;
   }
 
   while (curNode2 !== null) {
-    num2.push(curNode2.value);
+    const sum = curNode2.value + carry;
+
+    if (head === undefined) {
+      head = new Node(sum % 10);
+      curNode = head;
+    } else {
+      curNode.next = new Node(sum % 10);
+      curNode = curNode.next;
+    }
+
+    if (sum > 9) {
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+
     curNode2 = curNode2.next;
   }
 
-  const sum = (parseInt(num1.join(''), 10) + parseInt(num2.join(''), 10)).toString().split('');
-  const head = new Node(sum.slice(-1)[0]);
-  let curNode = head;
-
-  for (let i = sum.length - 2; i >= 0; i -= 1) {
-    curNode.next = new Node(sum[i]);
-    curNode = curNode.next;
+  if (carry === 1) {
+    curNode.next = new Node(1);
   }
+
+  // const num1 = [];
+  // const num2 = [];
+
+  // let curNode1 = l1;
+  // let curNode2 = l2;
+
+  // while (curNode1 !== null) {
+  //   num1.push(curNode1.value);
+  //   curNode1 = curNode1.next;
+  // }
+
+  // while (curNode2 !== null) {
+  //   num2.push(curNode2.value);
+  //   curNode2 = curNode2.next;
+  // }
+
+  // const sum = (parseInt(num1.join(''), 10) + parseInt(num2.join(''), 10)).toString().split('');
+  // const head = new Node(sum.slice(-1)[0]);
+  // let curNode = head;
+
+  // for (let i = sum.length - 2; i >= 0; i -= 1) {
+  //   curNode.next = new Node(sum[i]);
+  //   curNode = curNode.next;
+  // }
 
   return head;
 }
 
-// const ll1 = new Node(2);
-// const ll2 = new Node(1);
-// const ll3 = new Node(5);
+const ll1 = new Node(9);
+const ll2 = new Node(6);
+const ll3 = new Node(4);
 
-// ll1.next = ll2;
-// ll2.next = ll3;
+ll1.next = ll2;
+ll2.next = ll3;
 
-// const ll4 = new Node(5);
-// const ll5 = new Node(9);
-// const ll6 = new Node(2);
+const ll4 = new Node(7);
+const ll5 = new Node(0);
+const ll6 = new Node(1);
+const ll7 = new Node(1);
 
-// ll4.next = ll5;
-// ll5.next = ll6;
+ll4.next = ll5;
+ll5.next = ll6;
+ll6.next = ll7;
 
-// console.log(ll1);
-// console.log(ll4);
-// console.log(addLinkedList(ll1, ll4));
+console.log(JSON.stringify(ll1));
+console.log(JSON.stringify(ll4));
+console.log(JSON.stringify(addLinkedList(ll1, ll4)));
 
 module.exports = { Node, addLinkedList };
--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -1,5 +1,5 @@
 /**
- * I have an array where every number in the range 1...n appears once except for 
+ * I have an array where every number in the range 1...n appears once except for
  * one number which appears twice.
  *
  * Write a function for finding the number that appears twice.
@@ -11,7 +11,21 @@
  */
 
 function repeatNumbers(array) {
+  let max;
+  let sum = 0;
 
+  array.forEach((element) => {
+    if (max === undefined || element) {
+      max = element;
+    }
+    sum += element;
+  });
+
+  const sumFirstN = max * (max + 1) / 2;
+
+  return sum - sumFirstN;
 }
+
+// console.log(repeatNumbers([2, 1, 2, 3, 4]));
 
 module.exports = repeatNumbers;
