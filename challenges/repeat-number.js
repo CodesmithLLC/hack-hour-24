f95a6d45fb8cd33f1fa4b3b28176debc7a95a7ca
Garron Ma <garronma@gmail.com>
2018-11-01 09:15:26 -0700

    finish repeat-number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,15 @@
  */
 
 function repeatNumbers(array) {
+  const numObj = {};
 
+  for (let i = 0; i < array.length; i += 1) {
+    if (numObj[array[i]]) return array[i];
+    numObj[array[i]] = true;
+  }
+  return undefined;
 }
 
 module.exports = repeatNumbers;
+
+console.log(repeatNumbers([1, 2, 3, 2]));
