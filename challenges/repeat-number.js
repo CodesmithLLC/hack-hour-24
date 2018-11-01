26785e67d7520563ebc3c82c56c693713771c62f
Serge Vartanov <vartanov.s@gmail.com>
2018-11-01 09:20:28 -0700

    Repeat Number Challenge Completed

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -1,5 +1,5 @@
 /**
- * I have an array where every number in the range 1...n appears once except for 
+ * I have an array where every number in the range 1...n appears once except for
  * one number which appears twice.
  *
  * Write a function for finding the number that appears twice.
@@ -10,8 +10,28 @@
  *
  */
 
-function repeatNumbers(array) {
+// function repeatNumbers(array) {
+//   const numSet = new Set();
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (numSet.has(array[i])) return array[i];
+//     numSet.add(array[i]);
+//   }
+//   return false;
+// }
 
+// Strategy: Turn the number at array[array[i]] to a string
+// If it's already a string, the number must be a duplicate, return it
+function repeatNumbers(array) {
+  for (let i = 0; i < array.length; i += 1) {
+    // Check if the number at the array[i] is a string 
+    if (typeof array[Number(array[i])] === 'string') return array[i];
+    array[Number(array[i])] = String(array[Number(array[i])]);
+  }
+  return false;
 }
 
 module.exports = repeatNumbers;
+
+// console.log('---TESTING repeatNumbers---');
+// console.log(`repeatNumbers([1, 2, 3, 3, 4, 5]): expect -> 3: actual -> ${repeatNumbers([1, 2, 3, 3, 4, 5])}`);
+// console.log(`repeatNumbers([1, 2, 3, 4, 5, 1]): expect -> 1: actual -> ${repeatNumbers([1, 2, 3, 4, 5, 1])}`);
