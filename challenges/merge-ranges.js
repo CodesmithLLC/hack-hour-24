6881d04feaf69634b79f3e7bfca3153daa3a4e9f
Oscar Chan <chanoscar0@gmail.com>
2018-11-02 09:46:35 -0700

    merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,26 @@
 
 
 function mergeRanges(array) {
-
+  let sorted;
+  let range = [];
+  let output = [];
+  let rangeIndex = 0;
+  sorted = array.sort((a,b) => a[0] > b[0]);
+  console.log(array);
+  for (let i = 0; i < array.length; i += 1) {
+    if(output.length === 0) {
+      output.push(array[i]);
+    } else{
+      if(array[i][0] >= output[rangeIndex][0] && array[i][0] <= output[rangeIndex][1]){
+        output[rangeIndex][1] = array[i][1];
+      } else{
+        output.push(array[i]);
+        rangeIndex += 1;
+      }
+    }
+  }
+  return output;
 }
-
+var test = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+console.log(mergeRanges(test));
 module.exports = mergeRanges;
