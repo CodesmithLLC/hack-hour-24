2da6af6f3c834ffad70c496b5dc00ba687c37913
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-11-02 09:49:23 -0700

    superbalanced initial checkin

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -11,10 +11,28 @@ function BinaryTree(value) {
   this.value = value;
   this.left = null;
   this.right = null;
+  this.height;
 }
 
 function superbalanced(tree) {
-
+  if (tree.left === null && tree.right === null)
+    tree.height = 0;
+  else if (tree.left === null) {
+    if (!superbalanced(tree.right) || (tree.right.height > 1))
+      return false;
+    tree.height = tree.right.height + 1;
+  }
+  else if (tree.right === null) {
+    if (!superbalanced(tree.left) || (tree.left.height > 1))
+      return false;
+    tree.height = tree.left.height + 1;
+  }
+  else {
+    if (!superbalanced(tree.left) || !superbalanced(tree.right) || (Math.abs(tree.left.height - tree.right.height) > 1))
+      return false;
+    tree.height = Math.max(tree.left.height, tree.right.height) + 1;
+  }
+  return true;
 }
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
