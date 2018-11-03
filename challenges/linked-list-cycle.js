0db666f2f6a001fe3604e459b8c62aacf3104e3c
Oscar Chan <chanoscar0@gmail.com>
2018-11-03 09:30:31 -0700

    linked-list-cycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,26 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
-
+  const pastNodes = {};
+  let currNode = head;
+  while(currNode) {
+    if(pastNodes[currNode.value] && pastNodes[currNode.value].next === currNode.next) {
+      return true;
+    } else{
+      pastNodes[currNode.value] = currNode;
+    }
+    currNode = currNode.next;
+  }
+  return false;
 }
+
+  var node1 = new Node('1');
+  var node2 = node1.next = new Node('2');
+  var node3 = node2.next = new Node('3');
+  var node4 = node3.next = new Node('4');
+  var node5 = node4.next = new Node('5');
+  console.log(hasCycle(node1)); //false
+  node5.next = node2;
+  console.log(hasCycle(node1)); //true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
