47e8b8418c94eab0e4c32f8deded976fa812a769
Kevin Mui <kmui.work@gmail.com>
2018-11-06 11:04:23 -0800

    Complete max-subarray bruteforce.

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,52 @@
  */
 
 function maxSubarray(arr) {
+  // Bruteforce.
+  let start;
+  let end;
+  let max = -Infinity;
 
+  for (let i = 0; i < arr.length; i += 1) {
+    let curSum = 0;
+    for (let j = i; j < arr.length; j += 1) {
+      curSum += arr[j];
+      if (curSum > max) {
+        max = curSum;
+        start = i;
+        end = j;
+      }
+    }
+  }
+
+  return max;
 }
+
+// function maxSubarray2(arr) {
+//   let start;
+//   let end;
+//   let max = -Infinity;
+//   let curSum = 0;
+//   let i = 0;
+//   let j = 0;
+
+//   while (j < arr.length) {
+//     if (curSum + arr[j] < 0) {
+//       if (curSum > max) {
+//         max = curSum;
+//         start = i;
+//         end = j - 1;
+//       }
+//       curSum = arr[j];
+//       i = j;
+//     } else {
+//       curSum += arr[j];
+//     }
+//     j += 1;
+//   }
+
+//   return max;
+// }
+
+// console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
 
 module.exports = maxSubarray;
