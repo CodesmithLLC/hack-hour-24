99a1e2970445953bde2bab3c0b861da2824bd4f4
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-11-06 10:32:09 -0800

    max-subarray.js

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,30 @@
  */
 
 function maxSubarray(arr) {
+  let i = 0;
+  let sum = 0;
+  let start = 0;
+  let end = 0;
+  let max = Number.MIN_VALUE;
 
+  while (i < arr.length) {
+    if (sum === 0)
+      start = i;
+    sum += arr[i];
+    if (sum > max) {
+      max = sum;
+      end = i;
+    }
+    // reset sum if sum is negative
+    if (sum < 0) {
+      sum = 0;
+    }
+    i ++;
+  }
+  return arr.slice(start, end + 1);
 }
+
+console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
+console.log(maxSubarray([15,20,-5,10]))
 
 module.exports = maxSubarray;
