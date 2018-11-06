53ed2ea63d4e3bc81825b29b89f54d326a81f3aa
Serge Vartanov <vartanov.s@gmail.com>
2018-11-06 14:04:05 -0800

    Max Subarray Challenge Complete

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -7,8 +7,20 @@
  *
  */
 
-function maxSubarray(arr) {
+// Helper function that sums values in an array
+const sumArrVals = arr => arr.reduce((acc, val) => acc + val);
 
+function maxSubarray(arr) {
+  // Edge case, empty array
+  if (!arr.length) return 0;
+  // Terminal case, array has one value, return that value
+  if (arr.length === 1) return arr[0];
+  // Otherwise, return the max of the summed values and two recursive calls
+  return Math.max(sumArrVals(arr), maxSubarray(arr.slice(1)), maxSubarray(arr.slice(0, -1)));
 }
 
 module.exports = maxSubarray;
+
+console.log('---TESTING maxSubarray---');
+console.log(`maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]): expect -> 18 | actual -> ${maxSubarray([1, -2, 3, 10, -4, 7, 2, -5])}`);
+console.log(`maxSubarray([15, 20, -5, 10]): expect -> 40 | actual -> ${maxSubarray([15, 20, -5, 10])}`);
