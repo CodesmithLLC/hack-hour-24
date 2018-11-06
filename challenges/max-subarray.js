bb00c8aa0796d9a8f3c43e977687970d2b1309e8
Oscar Chan <chanoscar0@gmail.com>
2018-11-06 10:33:28 -0800

    done

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,26 @@
  */
 
 function maxSubarray(arr) {
-
+  let maxSum = Math.max();
+  let sumArray;
+  let runningSum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    runningSum += arr[i];
+    for(let x = i + 1; x < arr.length; x += 1){
+      runningSum += arr[x];
+      if(runningSum > maxSum) {
+        maxSum = runningSum;
+      }
+    }
+    runningSum = 0;
+  } 
+  return maxSum;
 }
+
+const test = [-15, -20, -5, -10];
+const test2 = [1, -2, 3, 10, -4, 7, 2, -5];
+
+console.log(maxSubarray(test));
+console.log(maxSubarray(test2))
 
 module.exports = maxSubarray;
