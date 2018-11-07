9954f0ff31ecb6072ec581d7ed392bb0aca32325
Serge Vartanov <vartanov.s@gmail.com>
2018-11-07 10:52:14 -0800

    Anagram Challenge Complete

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,31 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+const cache = {};
 
+function anagrams(string) {
+  // Check if answer has already been cached
+  if (cache[string]) return cache[string];
+  
+  // If string is a single character, return that character
+  if (string.length === 1) {
+    cache[string] = [string];
+    return [string];
+  }
+
+  // If string is a double character return both variants
+  if (string.length === 2) {
+    cache[string] = [string, string.split('').reverse().join()];
+  }
+
+  // Otherwise create and cache an array of anagrams
+  const anagramArray = [];
+  for (let i = 0; i < string.length; i += 1) {
+    const remainingChars = string.substring(0, i) + string.substring(i + 1);
+    anagrams(remainingChars).forEach(anagram => anagramArray.push(string[i] + anagram));
+  }
+  cache[string] = anagramArray;
+  return anagramArray;
 }
 
 module.exports = anagrams;
