e59be3bc5cee0e811c0730377cff091463911406
g-es <gesun@usc.edu>
2018-11-10 09:47:54 -0800

    find in ordered set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -10,9 +10,18 @@ findInOrderedSet(nums, 2);  -> false
  */
 
 
-function findInOrderedSet(arr, target) {
-
+function findInOrderedSet(arr, target, start = 0, end = arr.length - 1) {
+  if (target < arr[start]) return false;
+  if (target > arr[end]) return false;
+  const middle = Math.floor((start + end) / 2);
+  if (target === arr[middle]) return true;
+  if (target > arr[middle]) {
+    return findInOrderedSet(arr, target, middle + 1, end);
+  }
+  if (target < arr[middle]) {
+    return findInOrderedSet(arr, target, start, middle - 1);
+  }
 }
-
+// console.log(findInOrderedSet([1, 4, 6, 8, 9], 7));
 
 module.exports = findInOrderedSet;
