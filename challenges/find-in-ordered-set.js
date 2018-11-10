7fa0b50278f635bab3c3bdacd07e61ee2f5b45bd
Kevin Mui <kmui.work@gmail.com>
2018-11-10 09:35:20 -0800

    Completed find-in-ordered-set.js

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,29 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
+  // Binary search.
+  function binarySearch(arr, start, end, target) {
+    if (start > end) {
+      return false;
+    }
 
+    const mid = Math.floor((end + start) / 2);
+
+    if (arr[mid] === target) {
+      return true;
+    }
+
+    if (target < arr[mid]) {
+      return binarySearch(arr, start, mid - 1, target);
+    }
+    return binarySearch(arr, mid + 1, end, target);
+  }
+
+  return binarySearch(arr, 0, arr.length - 1, target);
 }
 
+const nums = [1, 4, 6, 7, 9, 17, 45];
+console.log(findInOrderedSet(nums, 4));
+console.log(findInOrderedSet(nums, 2));
 
 module.exports = findInOrderedSet;
