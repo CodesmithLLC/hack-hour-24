2553fb455742b1d86834767e89d1467c54bf866d
Serge Vartanov <vartanov.s@gmail.com>
2018-11-10 09:45:30 -0800

    Find In Ordered Set Challenge Complete

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -10,9 +10,31 @@ findInOrderedSet(nums, 2);  -> false
  */
 
 
-function findInOrderedSet(arr, target) {
-
-}
-
+const findInOrderedSet = (arr, target) => {
+  // Edge Case: Array is empty or single-item that doesn't match target
+  if (!arr.length || (arr.length === 1 && arr[0] !== target)) return false;
+  // Set placehodlers for array range under consideration
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    // Find the midpoint of the array (round down)
+    const midPoint = Math.floor((start - end) / 2);
+    // Check if this midpoint is the target - if so, return true
+    if (arr[midPoint + start] === target) return true;
+    // If the midpoint is higher than target, shift the end to the midPoint
+    if (arr[midPoint] > target) end = midPoint - 1;
+    // If the midpoint is lower than target, recursively call on the subset above the midpoint
+    else start = midPoint + 1;
+  }
+  return false;
+};
 
 module.exports = findInOrderedSet;
+
+// console.log('---TESTING findInOrderedSet---');
+// console.log(`findInOrderedSet([], 5): expect -> false: actual -> ${findInOrderedSet([], 5)}`);
+// console.log(`findInOrderedSet([3], 5): expect -> false: actual -> ${findInOrderedSet([3], 5)}`);
+// console.log(`findInOrderedSet([7], 5): expect -> false: actual -> ${findInOrderedSet([7], 5)}`);
+// console.log(`findInOrderedSet([5], 5): expect -> true: actual -> ${findInOrderedSet([5], 5)}`);
+// console.log(`findInOrderedSet([1, 2, 3, 4, 5], 1): expect -> true: actual -> ${findInOrderedSet([1, 2, 3, 4, 5], 1)}`);
+// console.log(`findInOrderedSet([1, 2, 3, 4, 5], 5): expect -> true: actual -> ${findInOrderedSet([1, 2, 3, 4, 5], 5)}`);
