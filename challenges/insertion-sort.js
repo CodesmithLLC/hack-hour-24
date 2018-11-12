b5b1cd7869e8975f297e3d11ec7a8b12c54a3b83
Oscar Chan <chanoscar0@gmail.com>
2018-11-12 09:31:14 -0800

    insertion-sort.js

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,24 @@
 
 
 function insertionSort(array) {
-
+  for(let i = 1; i < array.length; i += 1) {
+    let currElem = array.splice(i,1);
+    let insertPoint = i;
+    for(let x = i; x >= 0; x -= 1) {
+      if(currElem[0] < array[x]) {
+        insertPoint = x;
+      } 
+    }
+    array.splice(insertPoint, 0, currElem[0]);
+    console.log('Sorting...', array);
+  }
+  //splice curr elem
+  
+  //find what spot it needs to be in by looping through already sorted list
+  //insert it into that spot
 }
+let test = [14,3,7,12,8, 9 , 20, 1];
+insertionSort(test);
+console.log(test);
 
 module.exports = insertionSort;
