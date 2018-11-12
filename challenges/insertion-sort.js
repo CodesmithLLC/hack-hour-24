3d4dd09cb7c4d6f34eced3f8d711370a42bdec3d
Kevin Mui <kmui.work@gmail.com>
2018-11-12 09:21:34 -0800

    Completed insertion-sort.

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,22 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
-
 function insertionSort(array) {
-
+  // Sort in place.
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = i; j >= 1; j -= 1) {
+      if (array[j] < array[j - 1]) {
+        // Swap.
+        const temp = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = temp;
+      }
+    }
+  }
 }
+
+const array = [8, 5, 1, 3, 2, 4, 7, 10];
+insertionSort(array);
+console.log(array);
 
 module.exports = insertionSort;
