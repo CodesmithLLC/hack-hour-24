0ed71ffafd26306e1e9ef7fd50a281f11586b91f
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-11-12 09:34:11 -0800

    insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,21 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    for (let j = i; j >= 0; j--) {
+      // do exchange if array[i] is greater than array[j]
+      if (array[j] < array[j - 1]) {
+        let temp = array[j - 1];
+        array[j - 1] = array[j];
+        array[j] = temp;
+      }
+      else if (array[j] > array[j - 1])
+        break;
+    }
+  }
+  return array;
 }
 
+const array = [6, 5, 3, 1, 8, 7, 2, 4];
+console.log(insertionSort(array));
 module.exports = insertionSort;
