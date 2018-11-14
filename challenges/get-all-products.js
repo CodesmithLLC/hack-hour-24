695be0e1caa3e7632268bc4804f993f32f6ec133
Lee Rogers <edwinleerogers@gmail.com>
2018-11-14 09:15:00 -0800

    complete get-all-products

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,7 +10,19 @@
  */
 
 function getAllProducts(array) {
-
+  let result = [];
+  for (let i = 0; i < array.length; i += 1) {
+    let temp = 1;
+    for (let j = 0; j < array.length; j += 1) {
+      if (j !== i) {
+        temp *= array[j];
+      }
+    }
+    result.push(temp)
+  }
+  return result;
 }
+
+// console.log(getAllProducts([1, 7, 3, 4]));
 
 module.exports = getAllProducts;
