b336571abf742c4deef9b3123cf368cf0cda8572
Garron Ma <garronma@gmail.com>
2018-11-15 09:30:20 -0800

    finished doubly-linked-list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,64 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  const node = new Node(val);
+
+  if (!this.head) {
+    this.head = this.tail = node;
+  } else {
+    // tail node next points to new node
+    this.tail.next = node;
+
+    // assign prev of new node to tail node
+    node.prev = this.tail;
+
+    // reassign tail to new node
+    this.tail = node;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  let ptr = this.head;
+
+  while (ptr.val !== val || ptr === null) {
+    ptr = ptr.next;
+  }
+
+  if (ptr === null) return false;
+
+  // if first node
+  if (ptr === this.head) {
+    // reassign this.head to this.head.next
+    this.head = this.head.next;
+    // set new this.head.prev to null
+    this.head.prev = null;
+  }
+  // if last node
+  else if (ptr.next === null) {
+    // reassign this.tail to this.tail.prev
+    this.tail = this.tail.prev;
+    // set new this.tail.next to null
+    this.tail.next = null;
+  }
+  // else all other nodes
+  else {
+    // reassign found node next link onto prev node next link
+    ptr.prev.next = ptr.next;
+    // reassign found node next node prev to found node prev
+    ptr.next.prev = ptr.prev;
+  }
 };
 
 module.exports = LinkedList;
+
+const DLL = new LinkedList();
+
+DLL.add(1);
+DLL.add(2);
+DLL.add(3)
+DLL.remove(3);
+console.log(DLL)
