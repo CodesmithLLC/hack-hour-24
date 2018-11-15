fb67b7f909ae4843ef2c96ffa34c53255a464398
joelkperkins <joel.climbs@gmail.com>
2018-11-15 09:47:56 -0800

    double link

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,52 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  if (this.head === null) {
+    this.head = new Node(val);
+    this.tail = this.head;
+  }
+  else {
+    currNode = new Node(val);
+    this.tail.next = currNode;
+    currNode.prev = this.tail;
+    this.tail = currNode;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if (!this.head) return undefined; 
+  else {
+    currNode = this.head;
+    while (currNode !== null) {
+      //remove
+      if (currNode.val === val) {
+        // if we are removing the head
+        if (this.head === currNode) {
+          currNode.next.prev = null;
+          this.head = currNode.next;
+          return val;
+        }
+        // if we are removing the tail
+        else if (this.tail === currNode) {
+          currNode.prev.next = null;
+          this.tail = currNode.prev;
+          return val;
+        }
+        else {
+          currNode.next.prev = currNode.prev;
+          currNode.prev.next = currNode.next;
+          return val;
+        }
+      } 
+      else {
+        currNode = currNode.next;
+      }
+    }
+    return undefined; 
+  }
 };
 
 module.exports = LinkedList;
