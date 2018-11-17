c7a40cefee7fceca2807c04b7004d87eff31f099
Lee Rogers <edwinleerogers@gmail.com>
2018-11-17 09:18:41 -0800

    complete bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,16 @@
 
 
 function bubbleSort(array) {
-
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < array.length - i - 1; j += 1) {
+      if (array[j] > array[j+1]) {
+        const temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
