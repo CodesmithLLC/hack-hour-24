2de8de1ac8a417e488e1bc41c59077dc7099d96e
Garron Ma <garronma@gmail.com>
2018-11-17 09:21:10 -0800

    finish bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,22 @@
 
 
 function bubbleSort(array) {
+  const bubbled = array;
 
+  for (let pass = bubbled.length; pass >= 0; pass -= 1) {
+    for (let i = 1; i <= pass; i += 1) {
+      if (bubbled[i - 1] > bubbled[i]) {
+        let tmp = bubbled[i];
+        bubbled[i] = bubbled[i - 1];
+        bubbled[i - 1] = tmp;
+      }
+    }
+  }
+  return bubbled;
 }
 
 module.exports = bubbleSort;
+
+
+const array = [9, 4, 1, 2, 3];
+console.log(bubbleSort(array));
