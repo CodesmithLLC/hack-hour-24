8561eb8e0f649dde220651c6d40514d3047607f5
Kevin Mui <kmui.work@gmail.com>
2018-11-23 10:15:56 -0800

    Fixed get-all-products.

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,8 +10,8 @@
  */
 
 function getAllProducts(array) {
-  const sortedArr = array.sort((a, b) => a - b);
-  const products = [];
+  // const sortedArr = array.sort((a, b) => a - b);
+  // const products = [];
 
   if (array.length === 0) {
     return [0];
@@ -32,13 +32,33 @@ function getAllProducts(array) {
 
   // return products;
 
-  for (let i = 0; i < array.length; i += 1) {
+  // Three cases, no 0's, 1 0's, 2 or more 0's.
+  let zeroCount = 0;
+  let zeroPosition;
+  let product = 1;
 
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === 0) {
+      zeroCount += 1;
+      zeroPosition = i;
+    } else {
+      product *= array[i];
+    }
+  }
+
+  const products = Array(array.length).fill(0);
+
+  if (zeroCount === 1) {
+    products[zeroPosition] = product;
+  } else if (zeroCount === 0) {
+    return array.map(v => product / v);
   }
 
   return products;
 }
 
 console.log(getAllProducts([1, 7, 3, 4]));
+console.log(getAllProducts([1, 0, 3, 4]));
+console.log(getAllProducts([1, 0, 0, 4]));
 
 module.exports = getAllProducts;
