8a60718803b971b156244f43ef7f8c21b4f0e868
Ryan@CS <ryancs@Yang-Ryans-MacBook.local>
2018-11-26 16:59:28 -0800

    count-stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -13,9 +13,27 @@
  *
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
+function countStairsHelper(n, sub, solutions) {
+  let result = countStairs(n - sub);
+  for (let i = 0; i < result.length; i++) {
+    result[i].unshift(sub);
+    solutions.push(result[i]);
+  }
+  return solutions;
+}
 
 function countStairs(n) {
-
+  if (n === 0)
+    return [[]];
+  let solutions = [];
+  if ((n - 1) >= 0) {
+    solutions = countStairsHelper(n, 1, solutions);
+  }
+  if ((n - 2) >= 0) {
+    solutions = countStairsHelper(n, 2, solutions);
+  }
+  return solutions;
 }
+console.log(countStairs(5));
 
 module.exports = countStairs;
