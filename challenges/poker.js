c827f9b538942aa949ab34a680f83dc0acb0f10f
Serge Vartanov <vartanov.s@gmail.com>
2018-11-27 09:50:34 -0800

    Poker Challenge Complete

--- challenges/poker.js
+++ challenges/poker.js
@@ -19,8 +19,109 @@
  * BONUS: Account for suits and add in Flush & Straight Flush/Royal Flush.
  * BONUS2: Create a deck of cards function that generates two random hands for you.
  */
-function poker(hand1, hand2) {
 
+// Helper function - generate deck of 52 cards
+const generateDeck = () => {
+  const deck = [];
+  for (let i = 0; i < 4; i += 1) {
+    for (let j = 2; j < 15; j += 1) {
+      deck.push(j);
+    }
+  }
+  return deck;
+};
+
+// Helper function - generate two hands
+const generateHands = (deck) => {
+  const hand1 = [];
+  const hand2 = [];
+  // Loop through deck 5 times, slicing two cards out each time
+  for (let i = 0; i < 5; i += 1) {
+    let cardIndex = Math.floor(Math.random() * deck.length);
+    hand1.push(...deck.splice(cardIndex, 1));
+    cardIndex = Math.floor(Math.random() * deck.length);
+    hand2.push(...deck.splice(cardIndex, 1));
+  }
+  return [hand1, hand2];
+};
+
+// Helper function - Find straight, return high card
+const findStraight = (hand) => {
+  hand.sort((a, b) => a - b);
+  for (let i = 1; i < hand.length; i += 1) {
+    if (hand[i] - hand[i - 1] !== 1) return false;
+  }
+  return hand[hand.length - 1];
+};
+
+// Helper function - Find x of a kind, return repeating card
+const findOfAKind = (hand) => {
+  // Create dictionary of card frequency (card: freq)
+  const cards = {};
+  for (let i = 0; i < hand.length; i += 1) {
+    if (cards[hand[i]]) cards[hand[i]] += 1;
+    else cards[hand[i]] = 1;
+  }
+
+  // Identify count of most frequently seen card
+  const maxCardCount = Math.max(...Object.values(cards));
+
+  // Delete cards that don't match this frequency
+  Object.keys(cards).forEach((card) => {
+    if (cards[card] < maxCardCount) {
+      delete cards[card];
+    }
+  });
+
+  // Identify highest card with highest frequency
+  const maxCard = Math.max(...Object.keys(cards));
+
+  // Return frequency (X of a kind) and the card that has that frequency
+  return [maxCardCount, maxCard];
+};
+
+// Primary function - Compare two poker hands, identify winner
+function poker(hand1, hand2) {
+  // Check for straight and of a kinds
+  const hand1OfAKind = findOfAKind(hand1);
+  const hand2OfAKind = findOfAKind(hand2);
+  const hand1Straight = findStraight(hand1);
+  const hand2Straight = findStraight(hand2);
+
+  // Check if either player got 4 of a kind - if 4 of a kind found, determine and return winner
+  if (hand1OfAKind[0] === 4 && hand1OfAKind[0] === 4) {
+    return hand1OfAKind[1] > hand2OfAKind[1] ? 'Player 1 wins' : 'Player 2 wins';
+  }
+  if (hand1OfAKind[0] === 4) {
+    return 'Player 1 wins';
+  }
+  if (hand1OfAKind[1] === 4) {
+    return 'Player 2 wins';
+  }
+
+  // Check if either player got a straight - if straight found, determine and return winner
+  if (hand1Straight && hand2Straight) {
+    if (hand1Straight === hand2Straight) return 'Draw';
+    return hand1Straight > hand2Straight ? 'Player 1 wins' : 'Player 2 wins';
+  }
+  if (hand1Straight) return 'Player 1 wins';
+  if (hand2Straight) return 'Player 2 wins';
+
+  // Determine winner based on high of a kind frequency and card
+  if (hand1OfAKind[0] !== hand2OfAKind[0]) {
+    return hand1OfAKind[0] > hand2OfAKind[0] ? 'Player 1 wins' : 'Player 2 wins';
+  }
+  if (hand1OfAKind[1] !== hand2OfAKind[1]) {
+    return hand1OfAKind[1] > hand2OfAKind[1] ? 'Player 1 wins' : 'Player 2 wins';
+  }
+  return 'Draw';
 }
 
 module.exports = poker;
+
+// console.log('---TESTING poker---');
+// const newDeck = generateDeck();
+// const [newHand1, newHand2] = generateHands(newDeck);
+// console.log('Hand 1: ', newHand1);
+// console.log('Hand 2: ', newHand2);
+// console.log(poker(newHand1, newHand2));
