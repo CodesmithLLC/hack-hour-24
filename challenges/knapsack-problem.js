0de334ee1db362501fba514358545f688ab6945f
Oscar Chan <chanoscar0@gmail.com>
2018-11-30 09:32:34 -0800

    knapsack

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -10,7 +10,29 @@
 */
 
 function solveKnapsack(items, weightAvailable) {
-
+  if(items.length === 0) return 0;
+  if(weightAvailable <= 0) return 0;
+  let runningWeight = 0;
+  let runningValue = 0;
+  let checkIndex;
+  const values = [];
+  for(let i = 0; i < items.length; i += 1) {
+    checkIndex = i;
+    while(runningWeight < weightAvailable && checkIndex < items.length) {
+      runningWeight += items[checkIndex].weight;
+      if(runningWeight <= weightAvailable) {
+        runningValue += items[checkIndex].value;
+      }
+      checkIndex += 1;
+    }
+    values.push(runningValue);
+    console.log(values);
+    runningValue = 0;
+    runningWeight = 0;
+    checkIndex = 0;
+  }
+  return Math.max(...values);
 };
-
+let items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}, {weight:1, value: 6}];
+console.log(solveKnapsack(items, 1));
 module.exports = solveKnapsack;
