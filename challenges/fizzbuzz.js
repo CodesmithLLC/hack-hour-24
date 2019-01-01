b546001144f85d6d13084e4806420fb75cf3afa6
Garron Ma <garronma@gmail.com>
2019-01-01 17:33:10 +0800

    Finish fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,14 +17,29 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-  const fizzbuzzArr = [];
-  for (let i = 1; i <= num; i += 1) {
-    let word = '';
-    if (i % 3 === 0) { word += 'fizz'; }
-    if (i % 5 === 0) { word += 'buzz'; }
-    fizzbuzzArr.push(word.length > 0 ? word : i);
+  const fizzbuzzed = [];
+  let i = 1;
+
+  if (typeof num !== 'number') return fizzbuzzed;
+  while (i <= num) {
+    if (i % 15 === 0) fizzbuzzed.push('fizzbuzz');
+    else if (i % 5 === 0) fizzbuzzed.push('buzz');
+    else if (i % 3 === 0) fizzbuzzed.push('fizz');
+    else fizzbuzzed.push(i);
+    i += 1;
   }
-  return fizzbuzzArr;
+  return fizzbuzzed;
 }
+
+// function fizzbuzz(num) {
+//   const fizzbuzzArr = [];
+//   for (let i = 1; i <= num; i += 1) {
+//     let word = '';
+//     if (i % 3 === 0) { word += 'fizz'; }
+//     if (i % 5 === 0) { word += 'buzz'; }
+//     fizzbuzzArr.push(word.length > 0 ? word : i);
+//   }
+//   return fizzbuzzArr;
+// }
 
 module.exports = fizzbuzz;
