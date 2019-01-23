9984363ac060dbf9516bd6281f4ca00e00db5804
Lee Rogers <edwinleerogers@gmail.com>
2019-01-23 15:38:50 -0600

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -9,10 +9,11 @@
 
 function gcd(a, b) {
   const lesser = Math.min(a, b);
+  const greater = Math.max(a, b);
   for (let i = 1; i <= lesser; i += 1) {
     const divNum = lesser / i;
     if (divNum === Math.floor(divNum)) {
-      if (a / divNum === Math.floor(a / divNum)) {
+      if (greater / divNum === Math.floor(greater / divNum)) {
         return divNum;
       }
     }
