711647264faef7ecb556899f33f4ea033ed9ef3f
Lee Rogers <edwinleerogers@gmail.com>
2019-02-06 11:59:40 -0600

    merge-array

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -40,7 +40,7 @@ function mergeArrays(arr1, arr2) {
     if (arr1.length === 0 || (arr2[0] < arr1[0])) {
       result.push(arr2.shift());
     }
-    if (arr2.length === 0 || (arr1[0] < arr2[0])) {
+    if (arr2.length === 0 || (arr1[0] <= arr2[0])) {
       result.push(arr1.shift());
     }
   }
