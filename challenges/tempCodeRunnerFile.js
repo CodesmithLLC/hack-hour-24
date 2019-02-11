86d41f794b85d664b6226637aac5e01d7f19b266
Lee Rogers <edwinleerogers@gmail.com>
2019-02-10 18:42:18 -0600

    observed pin

---
+++ challenges/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+// function getPINs(observed) {
+//   const result = [''];
+//   const observedArr = observed.split('');
+//   observedArr.forEach((numStr) => {
+//     const possibleDigits = possibilities[Number(numStr)];
+//       let newnewSet = [];
+//     possibleDigits.forEach((digit) => {
+//       const newSet = [...result];
+//       newSet.forEach(comb => newnewSet.push(comb + digit);
+//       result.concat(newnewSet);
+//     });
+//   });
+//   return result;
+// }
