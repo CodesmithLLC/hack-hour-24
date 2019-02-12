0c7ed1c2b29107e59c2bc323c5efcf451a8c0a6a
Lee Rogers <edwinleerogers@gmail.com>
2019-02-12 01:21:35 -0600

    perm palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -11,7 +11,24 @@
 
 
 function permPalin(str) {
-	
+  const store = {};
+  const strArr = str.split('');
+  strArr.forEach((letter) => {
+    if (store[letter]) {
+      store[letter] += 1;
+    } else {
+      store[letter] = 1;
+    }
+  });
+  const storeKeys = Object.keys(store);
+  let oddLetters = 0;
+  storeKeys.forEach((k) => {
+    if (store[k] % 2 === 1) {
+      oddLetters += 1;
+    }
+  });
+  return oddLetters <= 1;
 }
+
 
 module.exports = permPalin;
